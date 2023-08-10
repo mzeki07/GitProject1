0ef218605ae5aa394e1861a5685ea3fc69bb9229
mzeki07 <zekiursavas07@gmail.com>
2023-08-09 20:55:34 -0400

    dont know th number commit

--- src/Day01/Animal.java
+++ src/Day01/Animal.java
@@ -1,4 +1,8 @@
 package Day01;
 
 public class Animal {
+    public static void main(String[] args) {
+        System.out.println("hello guys");
+        String name = "dip";
+    }
 }
--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -1,4 +1,10 @@
 package Day01;
 
 public class Student {
+    public static void main(String[] args) {
+        for (int i = 0; i <4 ; i++) {
+            System.out.println(i);
+            int age;
+        }
+    }
 }
